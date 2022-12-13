0bee84c30bcdfed5318fd4dccc31d5d32d94d502
DannyTang002 <dannytang002@gmail.com>
2022-12-13 11:25:20 +0100

    bad

--- day12/Advent12.java
+++ day12/Advent12.java
@@ -9,7 +9,7 @@ import inputread.InputListReader;
 
 public class Advent12 {
     private static char[][] grid;
-    private static List<Integer> length = new ArrayList<>();
+    private static List<String> length = new ArrayList<>();
     private static Map<Character, Integer> mapOfChar = new HashMap<>();
 
     public static void main(String[] args) {
@@ -17,6 +17,14 @@ public class Advent12 {
         InputListReader read = new InputListReader("day12/input12.txt");
         int x = 0;
         int y = 0;
+
+        String alfabet = "abcdefghijklmnopqrstuvwxyz";
+        char[] alfabetSeq = alfabet.toCharArray();
+        int sum = 0;
+        for (char c : alfabetSeq) {
+            sum++;
+            mapOfChar.put(c, sum);
+        }
 
         List<String> file = read.file();
         grid = new char[file.size()][file.get(0).length()];
@@ -33,35 +41,46 @@ public class Advent12 {
             }
             System.out.println();
         }
-        String alfabet = "abcdefghijklmnopqrstuvwxyz";
-        char[] alfabetSeq = alfabet.toCharArray();
-        int sum = 0;
-        for (char c : alfabetSeq) {
-            sum++;
-            mapOfChar.put(c, sum);
-        }
+
         mapOfChar.put('S', 0);
+        mapOfChar.put('E', 1000);
 
         boolean check = true;
 
         System.out.println(grid[y][x]);
+        length = paths(x, y);
+
+        for (String j : length) {
+            System.out.println(j);
+        }
 
     }
 
-    public static void char(int x, int y) {
+    public static List<String> paths(int x, int y) {
+        List<String> paths = new ArrayList<>();
+        move(x, y, paths, "");
+        return paths;
+    }
 
-        for (int i = y - 1; i <= y + 1; i++) {
+    public static void move(int x, int y, List<String> paths, String path) {
 
-            for (int j = x - 1; j <= x + 1; j++) {
-
-                if (mapOfChar.get(grid[i][j]) >= mapOfChar.get(grid[y][x]) && i != y && j != x) {
-
+        path += grid[y][x];
+        System.out.println(grid[y][x]);
+        System.out.println(path);
+        if (grid[y][x] == 'E') {
+            paths.add(path);
+        } else {
+            for (int i = y - 1; i < y + 1; i += 2) {
+                for (int j = x - 1; j < j + 1; j += 2) {
+                    if (i < grid.length && j < grid[y].length && j >= 0 && i >= 0) {
+                        if (mapOfChar.get((grid[i][j])) >= mapOfChar.get(grid[x][y])) {
+                            System.out.println("y " + i + " x " + j);
+                            move(j, i, paths, path);
+                        }
+                    }
                 }
-
             }
-
         }
-
     }
 
 }
